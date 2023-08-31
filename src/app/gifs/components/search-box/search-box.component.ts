af401b6a9b8c59182434a472d4bc0b02f1ebe4df
javier perez osorio <japere@bancolombia.com.co>
2023-08-30 19:32:57 -0500

    feat: service

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -8,12 +9,25 @@ import { Component, OnInit } from '@angular/core';
     type="text"
     class="form-control"
     placeholder="Buscar gifs"
+    (keyup.enter)="searchTag()"
+    #txtTagInput
     >
   `
 })
 
-export class SearchBoxComponent implements OnInit {
-  constructor() { }
+export class SearchBoxComponent{
 
-  ngOnInit() { }
+  @ViewChild('txtTagInput')
+  tagInput!:ElementRef<HTMLInputElement>
+
+
+  constructor(private gifsService:GifsService) { }
+
+  searchTag(){
+    const newTag=this.tagInput.nativeElement.value;
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
+
+  }
+
 }
