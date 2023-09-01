75999e31df852d709c3785e13e08cdc25ceb69fc
javier perez osorio <japere@bancolombia.com.co>
2023-08-31 19:38:21 -0500

    feat: next class

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,20 +1,64 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class GifsService {
+  private apikey: string = 'EnFWRTLnnkSQkrqa1iJJTwUhMSxFrY9b';
+  private serviUrl: string = "https://api.giphy.com/v1/gifs";
+  private _tagsHistory: string[] = [];
+  public gifList:Gif[] = [];
 
-  private _tagsHistory:string[] = [];
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-
-  get tagsHistory(){
+  get tagsHistory() {
     return [...this._tagsHistory]
   }
 
-
-  public searchTag(tag:string):void{
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldtag) => oldtag !== tag)
+    }
+    this._tagsHistory = this._tagsHistory.splice(0, 10)
     this._tagsHistory.unshift(tag);
+  }
+
+  searchTag(tag: string): void {
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apikey)
+      .set('limit', '10')
+      .set('q', tag)
+
+    this.http.get<SearchResponse>(`${this.serviUrl}/search`, { params })
+      .subscribe(resp => {
+
+        this.gifList = resp.data;
+
+        console.log({gifs: this.gifList})
+      })
 
   }
+
+  // async searchTag(tag:string):Promise<void>{
+
+  //   if(tag.length===0) return;
+  //   this.organizeHistory(tag);
+  //   this.http.get('https://api.giphy.com/v1/gifs/search?api_key=EnFWRTLnnkSQkrqa1iJJTwUhMSxFrY9b&q=valorant&limit=10')
+
+  //   // fetch('https://api.giphy.com/v1/gifs/search?api_key=EnFWRTLnnkSQkrqa1iJJTwUhMSxFrY9b&q=valorant&limit=10')
+  //   // .then(resp => resp.json())
+  //   // .then(data=> console.log(data))
+
+  //   // const resp=await fetch('https://api.giphy.com/v1/gifs/search?api_key=EnFWRTLnnkSQkrqa1iJJTwUhMSxFrY9b&q=valorant&limit=10')
+  //   // const data=await resp.json();
+  //   // console.log(data)
+  // }
+
+
+
 
 }
